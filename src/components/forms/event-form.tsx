45bb0f1134e0cf0de9d2e064c342591e7224cdc4
darrny <darren.lim.off@gmail.com>
2024-12-18 20:32:10 +0800

    Updated dependencies

--- src/components/forms/event-form.tsx
+++ src/components/forms/event-form.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import { Button } from '@/components/ui/button';
-import { Calendar } from '@/components/ui/calendar';
 import {
     Popover,
     PopoverContent,
